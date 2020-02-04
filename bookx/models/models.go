f0c58ceeb9c7c03e9f89de5e00fc51c9546b8aa5
Humaun Kabir <psychopath.humaun@gmail.com>
2020-02-04 21:50:00 +0600

    Struct Changed

--- bookx/models/models.go
+++ bookx/models/models.go
@@ -4,14 +4,10 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 
 type Book struct {
-	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Publisher   string        `json:"psher,omitempty" bson:"psher,omitempty"`
-	Year string 			  `json:"year,omitempty" bson:"year,omitempty"`
-	Title  string             `json:"title" bson:"title,omitempty"`
-	Author *Author            `json:"author" bson:"author,omitempty"`
-}
-
-type Author struct {
-	FirstName string `json:"firstname,omitempty" bson:"firstname,omitempty"`
-	LastName  string `json:"lastname,omitempty" bson:"lastname,omitempty"`
+	ID     primitive.ObjectID 	`json:"_id,omitempty" bson:"_id,omitempty"`
+    Title  string       `json:"title,omitempty" bson:"title,omitempty"`
+	Author string       `json:"author, omitempty" bson:"author,omitempty"`
+	Publisher   string  `json:"psher,omitempty" bson:"psher,omitempty"`
+	Year string 		`json:"year,omitempty" bson:"year,omitempty"`
+	Category string 	`json:"cat, omitempty" bson: "cat, omitempty"`
 }
