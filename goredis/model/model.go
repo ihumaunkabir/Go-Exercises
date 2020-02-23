e4dfcca84544b01f832b04c67a306cba8e43e5a1
Humaun Kabir <psychopath.humaun@gmail.com>
2020-02-24 00:07:54 +0600

    person struct created

---
+++ goredis/model/model.go
@@ -0,0 +1,10 @@
+package model
+
+type Person struct{
+
+	Name string `json: "name, omitempty`
+	Age int64 `json: "age, omitempty"`
+	City string `json: "city, omitempty`
+	Country string `json: "country, omitempty`
+
+}
